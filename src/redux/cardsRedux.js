41bcb3766e5ae3777d76683410aee3287e45bb09
Mateusz Olszewski <matcrono@gmail.com>
2021-03-18 21:36:35 +0100

    Fixed double code at cardsRedux

--- src/redux/cardsRedux.js
+++ src/redux/cardsRedux.js
@@ -24,23 +24,3 @@ export default function reducer(statePart = [], action = {}) {
   }
 }
 
-
-import shortid from 'shortid';
-
-export const getCardsforColumn = ({ cards }, columnId) => cards.filter(card => card.columnId == columnId);
-
-const reducerName = 'cards';
-const createActionName = name => `app/${reducerName}/${name}`;
-
-export const ADD_CARD = createActionName('ADD_CARD');
-
-export const createActionAddCard = payload => ({ payload: { ...payload, id: shortid.generate() }, type: ADD_CARD });
-
-export default function reducer(statePart = [], action = {}) {
-    switch (action.type) {
-        case ADD_CARD:
-            return [...statePart, action.payload];
-        default:
-            return statePart;
-    }
-}
